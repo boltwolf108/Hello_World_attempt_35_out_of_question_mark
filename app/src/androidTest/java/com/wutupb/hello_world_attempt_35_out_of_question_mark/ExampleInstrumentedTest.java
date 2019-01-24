4c030b75c3bd55f835ed6431b3bd0ad46a30aa56
Levy Ford <lford@windows.linburg.com>
2019-01-24 10:13:20 -0500

    Initial commit

---
+++ app/src/androidTest/java/com/wutupb/hello_world_attempt_35_out_of_question_mark/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package com.wutupb.hello_world_attempt_35_out_of_question_mark;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("com.wutupb.hello_world_attempt_35_out_of_question_mark", appContext.getPackageName());
+    }
+}
---
+++ app/src/test/java/com/wutupb/hello_world_attempt_35_out_of_question_mark/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.wutupb.hello_world_attempt_35_out_of_question_mark;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
